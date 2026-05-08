321f20e3f15bd1fbc21c90831bfc3dd93351ec87
Minduli lasandi <mindulilasandi@gmail.com>
2026-05-08 17:07:34 +0530

    Add data files

---
+++ src/data/publications.ts
@@ -0,0 +1,30 @@
+export const bibtexEntries = `
+
+@inproceedings{lasandi-jayatilleke-2026-sinhalegal,
+    title = "{S}inha{L}egal: A Benchmark Corpus for Information Extraction and Analysis in {S}inhala Legislative Texts",
+    author = "Lasandi, Minduli  and
+      Jayatilleke, Nevidu",
+    editor = "Hettiarachchi, Hansi  and
+      Ranasinghe, Tharindu  and
+      Plum, Alistair  and
+      Rayson, Paul  and
+      Mitkov, Ruslan  and
+      Gaber, Mohamed  and
+      Premasiri, Damith  and
+      Tan, Fiona Anting  and
+      Uyangodage, Lasitha",
+    booktitle = "Proceedings of the Second Workshop on Language Models for Low-Resource Languages ({L}o{R}es{LM} 2026)",
+    month = mar,
+    year = "2026",
+    address = "Rabat, Morocco",
+    publisher = "Association for Computational Linguistics",
+    url = "https://aclanthology.org/2026.loreslm-1.11/",
+    doi = "10.18653/v1/2026.loreslm-1.11",
+    pages = "114--131",
+    ISBN = "979-8-89176-377-7",
+    abstract = "SinhaLegal introduces a Sinhala legislative text corpus containing approximately 2 million words across 1,206 legal documents. The dataset includes two types of legal documents: 1,065 Acts dated from 1981 to 2014 and 141 Bills from 2010 to 2014, which were systematically collected from official sources. The texts were extracted using OCR with Google Document AI, followed by extensive post-processing and manual cleaning to ensure high-quality, machine-readable content, along with dedicated metadata files for each document. A comprehensive evaluation was conducted, including corpus statistics, lexical diversity, word frequency analysis, named entity recognition, and topic modelling, demonstrating the structured and domain-specific nature of the corpus. Additionally, perplexity analysis using both large and small language models was performed to assess how effectively language models respond to domain-specific texts. The SinhaLegal corpus represents a vital resource designed to support NLP tasks such as summarisation, information extraction, and analysis, thereby bridging a critical gap in Sinhala legal research."
+}
+
+
+
+`
