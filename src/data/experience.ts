643bff31cdf188399569cc3a116cec3fde802258
Minduli lasandi <mindulilasandi@gmail.com>
2026-05-09 00:03:30 +0530

    Add experience

---
+++ src/data/experience.ts
@@ -0,0 +1,10 @@
+export const experience = [
+    {
+        role: "Undergraduate Trainee",
+        company: " IFS R&D International (Pvt.) Ltd",
+        duration: "July 2024 – June 2025",
+        description: "Contributed to defect fixing and small-scale feature enhancements for enterprise applications using TypeScript and Angular. Worked with PL/SQL and internal IFS technologies, participated in unit and Cypress testing, and supported backward compatibility and service update testing within agile development workflows.",
+        tags: ["Angular", "Typescript", "PL/SQL", "Marble (Internal language at IFS)", "Git bisecting", "Cypress testing", "Unit testing", "Backward compatibilty and service update testing"]
+    },
+
+]
