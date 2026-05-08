439a47b69b294f0dd79edd7b81a68c76cbb7e5a7
Minduli lasandi <mindulilasandi@gmail.com>
2026-05-08 17:15:41 +0530

    Add skills

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -25,6 +25,4 @@ export const bibtexEntries = `
     abstract = "SinhaLegal introduces a Sinhala legislative text corpus containing approximately 2 million words across 1,206 legal documents. The dataset includes two types of legal documents: 1,065 Acts dated from 1981 to 2014 and 141 Bills from 2010 to 2014, which were systematically collected from official sources. The texts were extracted using OCR with Google Document AI, followed by extensive post-processing and manual cleaning to ensure high-quality, machine-readable content, along with dedicated metadata files for each document. A comprehensive evaluation was conducted, including corpus statistics, lexical diversity, word frequency analysis, named entity recognition, and topic modelling, demonstrating the structured and domain-specific nature of the corpus. Additionally, perplexity analysis using both large and small language models was performed to assess how effectively language models respond to domain-specific texts. The SinhaLegal corpus represents a vital resource designed to support NLP tasks such as summarisation, information extraction, and analysis, thereby bridging a critical gap in Sinhala legal research."
 }
 
-
-
 `
---
+++ src/data/skills.ts
@@ -0,0 +1,14 @@
+export const skills = [
+    {
+        category: "Languages and Frameworks",
+        items: ["Python", "Flask", "Angular", "TypeScript", "Java", "R", "Flutter", "Dart", "JavaScript"]
+    },
+    {
+        category: "Libraries",
+        items: ["PyTorch", "Scikit-learn", "Pandas", "NumPy", "NetworkX", "Matplotlib,", "NLTK", ""]
+    },
+    {
+        category: "Tools",
+        items: ["Git", "Docker", "HuggingFace Transformers"]
+    },
+]
