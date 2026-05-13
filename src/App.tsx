4a443870d5c174660fd57d665380f25a52730e21
Minduli lasandi <mindulilasandi@gmail.com>
2026-05-13 07:12:20 +0530

    Update css and app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Education from './components/Education'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
 import Publications from './components/Publications'
@@ -11,13 +12,14 @@ export default function App() {
       <Navbar />
       <main>
         <Hero />
+        <Education />
         <Experience />
         <Projects />
         <Publications />
         <Skills />
       </main>
       <footer className="footer">
-        <p> Minduli Lasandi Portfolio</p>
+        <p>Minduli Lasandi Portfolio</p>
       </footer>
     </>
   )
