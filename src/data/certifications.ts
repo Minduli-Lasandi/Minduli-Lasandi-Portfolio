1b86715c22c5c7c8d3245f76d05881a2784a4bfb
Minduli lasandi <mindulilasandi@gmail.com>
2026-05-13 16:10:09 +0530

    Add certifications section

---
+++ src/data/certifications.ts
@@ -0,0 +1,34 @@
+export const certifications = [
+    {
+        name: "Java Algorithms",
+        issuer: "LinkedIn",
+        date: "Aug 2023",
+        link: "https://www.linkedin.com/learning/certificates/a3f08627f20a9bc699fad9169d2bf80b1c009520803cab26d22bab21ca491129?u=76664938"
+    },
+
+    {
+        name: "Java Essential Training: Objects and APIs",
+        issuer: "LinkedIn",
+        date: "Sep 2023",
+        link: "https://www.linkedin.com/learning/certificates/809033c7efb865bc2223b8815d0485f08fa8f9b6c0824ea437c823f428bc4443?u=76664938"
+    },
+    {
+        name: "JavaScript Essential Training",
+        issuer: "LinkedIn",
+        date: "Feb 2024",
+        link: "https://www.linkedin.com/learning/certificates/e9ec7a65df9e70341ef5dcec4cbdffd483c9949f3b56bc3bc2dfddcdb0f7bcb1?u=76664938"
+    },
+    {
+        name: "Machine Learning with Python: Foundations",
+        issuer: "LinkedIn",
+        date: "Sep 2023",
+        link: "https://www.linkedin.com/learning/certificates/fc26d416998d981a792704a48ec83aacce528cc46bcfcc3c0b779c882efba3b2?u=76664938"
+    },
+    {
+        name: "Natural Language Processing for Speech and Text From Beginner to Advanced",
+        issuer: "LinkedIn",
+        date: "Ongoing",
+        link: ""
+    },
+
+]
