a0755a3e0534182755cb028add2a755f95af84e6
Minduli lasandi <mindulilasandi@gmail.com>
2026-05-26 15:57:25 +0530

    Update skills and resume

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,14 +1,14 @@
 export const skills = [
     {
         category: "Languages and Frameworks",
-        items: ["Python", "Flask", "Angular", "TypeScript", "Java", "R", "Flutter", "Dart", "JavaScript", "Cypress"]
+        items: ["Python", "Flask", "Angular", "TypeScript", "Java", "R", "Flutter", "Dart", "JavaScript", "Cypress", "FastAPI"]
     },
     {
         category: "Libraries",
-        items: ["PyTorch", "Scikit-learn", "Pandas", "NumPy", "NetworkX", "Matplotlib,", "NLTK", "HuggingFace Transformers"]
+        items: ["PyTorch", "Scikit-learn", "Pandas", "NumPy", "NetworkX", "Matplotlib,", "joblib", "NLTK", "HuggingFace Transformers"]
     },
     {
         category: "Tools",
-        items: ["Git", "Docker", "Gradio"]
+        items: ["Git", "Docker", "Gradio", "Uvicorn"]
     },
 ]
