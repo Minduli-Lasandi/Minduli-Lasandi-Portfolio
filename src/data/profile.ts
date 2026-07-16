e7b2f2f332dac81f356ffde9b95c7c8d72487244
Minduli lasandi <mindulilasandi@gmail.com>
2026-07-16 12:36:53 +0530

    update header

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,6 +1,6 @@
 export const profile = {
     name: "Minduli Lasandi",
-    title: "Final Year Undergraduate @ IIT | ML/NLP Research Enthusiast",
+    title: "Recent Grauduate @ IIT | Software Engineer | Research Enthusiast",
     bio: "I am currently a final-year undergraduate pursuing a Software Engineering degree at the Informatics Institute of Technology (IIT), affiliated with the University of Westminster. I have a deep enthusiasm for learning new things and staying in touch with the latest technologies.  I have recently begun exploring the field of Natural Language Processing (NLP) and am developing a strong interest in it. Beyond academic and professional pursuits, I believe in the importance of inspiration, and enjoy writing to bring a sense of motivation to others.",
     email: "mindulilasandi@gmail.com",
     links: {
